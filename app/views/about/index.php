9449044e03a416cc11eadea46ea8e47c91efe2cb
Zulkifli Sabili Rosad <enjoelzul92@gmail.com>
2026-02-04 14:11:00 +0700

    belajar assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,5 @@
-
-    <h1>About Me</h1>
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?=  BASEURL; ?>/img/izul.jpg" alt="Zulkifli Sabili Rosad" class="rounded-cicrle shadow" width="200">
     <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?>, saya adalah seorang <?= $data['pekerjaan']; ?></p>
+</div>
