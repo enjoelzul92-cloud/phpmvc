5b9040f7d017f389d917aae7ebee7d6c9373fdd6
Zulkifli Sabili Rosad <enjoelzul92@gmail.com>
2026-02-06 10:11:49 +0700

    update assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,5 @@
-<h1>About Me</h1>
-<p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?>, dan pekerjaan saya adalah <?= $data['pekerjaan']; ?></p>
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASE_URL ?>/img/profile.jpg" alt="Zulkifli Sabili Rosad" width="200" class="rounded-circle shadow">
+    <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?>, dan pekerjaan saya adalah <?= $data['pekerjaan']; ?></p>
+</div>
